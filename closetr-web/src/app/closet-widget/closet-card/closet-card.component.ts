c09fa1cc9bac3b428bfcda7f23bbf770b4217bd7
desslinga <fides.linga@mail.utoronto.ca>
2018-11-28 22:16:54 -0500

    S-3: separating closet card into components, refactoring some scss files.

---
+++ closetr-web/src/app/closet-widget/closet-card/closet-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-closet-card',
+  templateUrl: './closet-card.component.html',
+  styleUrls: ['./closet-card.component.scss']
+})
+export class ClosetCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
