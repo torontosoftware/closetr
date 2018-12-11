5384650ec79db39a49fd5f8171809eebf516a9cf
desslinga <fides.linga@mail.utoronto.ca>
2018-12-10 22:04:10 -0500

    S-7: Adding back button to add clothing page, and animation

--- closetr-web/src/app/add-clothing/add-clothing.component.ts
+++ closetr-web/src/app/add-clothing/add-clothing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-add-clothing',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddClothingComponent implements OnInit {
 
-  constructor() { }
+  back(): void {
+    this._location.back();
+  }
+
+  constructor(private _location: Location) { }
 
   ngOnInit() {
   }
