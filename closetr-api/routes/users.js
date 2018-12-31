4107c9477656c29a1dcdeeb48e7509f0e5bfdb78
desslinga <fides.linga@mail.utoronto.ca>
2018-12-30 19:34:56 -0500

    got mongoose

--- closetr-api/routes/users.js
+++ closetr-api/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//get closet api
+router.get('/api/gc', function(req, res, next) {
+  res.send('hi there!');
+});
+
 module.exports = router;
