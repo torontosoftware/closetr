a1138d08b1c0c54d55c8c96f45f90102e5b4653f
Fides Linga <fides.linga@mail.utoronto.ca>
2018-12-31 12:34:05 -0500

    S-12: ongoing change to use angular animations

--- closetr-web/src/app/base-general/base-general.component.ts
+++ closetr-web/src/app/base-general/base-general.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { trigger, style, transition, animate, group } from '@angular/animations';
 
 @Component({
   selector: 'app-base-general',
-  templateUrl: './base-general.component.html',
-  styleUrls: ['./base-general.component.scss']
+  template : '',
+  styleUrls: ['./base-general.component.scss'],
+  animations: [
+  trigger('itemAnim', [
+      transition(':enter', [
+        style({transform: 'translateX(-100%)'}),
+        animate(350)
+      ]),
+      transition(':leave', [
+        group([
+          animate('0.2s ease', style({
+            transform: 'translate(150px,25px)'
+          })),
+          animate('0.5s 0.2s ease', style({
+            opacity: 0
+          }))
+        ])
+      ])
+    ])
+  ]
 })
-export class BaseGeneralComponent implements OnInit {
-
-  constructor() { }
+export class BaseGeneralComponent implements OnInit, OnDestroy {
+  exitClass: boolean
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
   }
 
 }
