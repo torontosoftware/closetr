465ee0688163406163fb63a20ee1ed542083bc86
Fides Linga <fides.linga@mail.utoronto.ca>
2018-12-31 12:34:05 -0500

    S-12: ongoing change to use angular animations

--- closetr-web/src/app/app.module.ts
+++ closetr-web/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -37,6 +39,7 @@ import { BaseGeneralComponent } from './base-general/base-general.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule
   ],
