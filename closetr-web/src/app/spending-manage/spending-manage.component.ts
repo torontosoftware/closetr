6377e8a80c500418de35ff68bbeeebd3a5d85b78
Fides Linga <fides.linga@mail.utoronto.ca>
2019-01-04 11:37:38 -0500

    S-14: Added initial UI design for spending manage UI

--- closetr-web/src/app/spending-manage/spending-manage.component.ts
+++ closetr-web/src/app/spending-manage/spending-manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
 
 @Component({
   selector: 'app-spending-manage',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spending-manage.component.scss']
 })
 export class SpendingManageComponent implements OnInit {
+  purchaseList: object;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.purchaseList = [
+      {price:'$15',name:'Aritzia TShirt'},
+      {price: '$399', name:'The Stowe Bag'},
+      {price: '13', name:'UO Blouse'},
+      {price:'$35',name:'Uniqlo Sweater'},
+      {price:'$5',name:'Uniqlo Socks'},
+      {price:'$15',name:'Aritzia TShirt'}
+      {price:'$35',name:'Uniqlo Sweater'}
+    ];
+    console.log(this.purchaseList);
+  }
+
+  back(): void {
+    this.router.navigate(['/dashboard']);
+  }
 
   ngOnInit() {
   }
