942b57bd17c098930fe766ecc883c714e91c807f
Fides Linga <fides.linga@mail.utoronto.ca>
2019-01-23 15:12:09 -0500

    S-15: Added back button, routing to budget manage, and date range options

--- closetr-web/src/app/budget-manage/budget-manage.component.ts
+++ closetr-web/src/app/budget-manage/budget-manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
 
 @Component({
   selector: 'app-budget-manage',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-manage.component.scss']
 })
 export class BudgetManageComponent implements OnInit {
+  availableDateRange: Array<string>;
+  budgetSettings: any;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.availableDateRange = [
+      'week',
+      'two weeks',
+      'month',
+      '6 months',
+      'year'
+    ];
+  }
+
+  back(): void {
+    this.router.navigate(['/spending-manage']);
+  }
 
   ngOnInit() {
   }
