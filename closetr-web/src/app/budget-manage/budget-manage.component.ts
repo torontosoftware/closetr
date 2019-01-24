f45fabf99e57c7bbf536b533bfe9e2f117dd3b31
Fides Linga <fides.linga@mail.utoronto.ca>
2019-01-24 11:39:31 -0500

    S-15: Enabled adding of new budget spans.

--- closetr-web/src/app/budget-manage/budget-manage.component.ts
+++ closetr-web/src/app/budget-manage/budget-manage.component.ts
@@ -7,24 +7,48 @@ import { RouterModule, Routes, Router } from '@angular/router';
   styleUrls: ['./budget-manage.component.scss']
 })
 export class BudgetManageComponent implements OnInit {
-  availableDateRange: Array<string>;
+  availableBudgetSpans: Array<string>;
+  allBudgetSpans: Array<string>;
+  selectedBudgetSpans: Array<string>;
+  selectedBudgetSpan: string;
   budgetSettings: any;
 
   constructor(private router: Router) {
-    this.availableDateRange = [
+    this.allBudgetSpans = [
+      'select',
       'week',
       'two weeks',
       'month',
       '6 months',
       'year'
     ];
+
+    this.selectedBudgetSpans = [
+      'month'
+    ];
+
+    this.selectedBudgetSpan = 'select';
+  }
+
+  ngOnInit() {
+    // take availableBudgetSpans as differences between all date ranges
+    // and selected date ranges.
+    this.availableBudgetSpans = this.allBudgetSpans.filter(
+      budgetSpan => !this.selectedBudgetSpans.includes(budgetSpan)
+    );
+  }
+
+  addBudgetSpan(budgetSpan: string): void {
+    console.log(budgetSpan);
+    this.selectedBudgetSpans.push(budgetSpan);
+    this.availableBudgetSpans = this.allBudgetSpans.filter(
+      budgetSpan => !this.selectedBudgetSpans.includes(budgetSpan)
+    );
+    this.selectedBudgetSpan = 'select';
   }
 
   back(): void {
     this.router.navigate(['/spending-manage']);
-  }
-
-  ngOnInit() {
   }
 
 }
