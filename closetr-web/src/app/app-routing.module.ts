124128522c977910bbd44658fc703d3088122183
desslinga <fides.linga@mail.utoronto.ca>
2019-01-28 18:41:49 -0500

    S-25: Configured routing to start at login component.

--- closetr-web/src/app/app-routing.module.ts
+++ closetr-web/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ClosetManageComponent } from './closet-manage/closet-manage.component';
 import { AddClothingComponent } from './add-clothing/add-clothing.component';
@@ -11,8 +12,9 @@ import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
   {path: 'log-outfit', component: LogOutfitComponent},
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'add-clothing', component: AddClothingComponent},
   {path: 'closet-manage', component: ClosetManageComponent},
