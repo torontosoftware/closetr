cda746d27c3e276a50f1864552ff9bf44e5fb936
Fides Linga <fides.linga@mail.utoronto.ca>
2019-01-29 16:35:59 -0500

    S-26: Added validation to register component, and fixed compilation issue.

--- closetr-web/src/app/register/register.component.ts
+++ closetr-web/src/app/register/register.component.ts
@@ -6,8 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  name: string;
+  username: string;
+  password: string;
+  passwordConfirm: string;
+  enableLogin: boolean;
 
-  constructor() { }
+  constructor() {
+    this.name = "";
+    this.username = "";
+    this.password = "";
+    this.passwordConfirm = "";
+  }
+
+  checkEnableRegister(): boolean {
+    if (this.name.length == 0
+        || this.username.length == 0
+        || this.password.length == 0
+        || this.passwordConfirm.length == 0
+        || (this.password != this.passwordConfirm)) {
+      return false;
+    }
+    return true;
+  }
 
   ngOnInit() {
   }
