ba976c56a7f2ed22c1626956ca24f7380794e604
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-04 14:10:59 -0500

    S-31: Created and documented clothing model.

---
+++ closetr-web/src/app/models/clothing.model.ts
@@ -0,0 +1,93 @@
+export class Clothing {
+  clothingID: string;
+  clothingName: string;
+  clothingWorn: number;
+  clothingCost: number;
+  clothingCategory: string;
+  clothingPurchaseDate: string;
+
+  /*
+  get clothingID
+  */
+  getClothingID(): string {
+    return this.clothingID;
+  }
+
+  /*
+  set clothingID
+  */
+  setClothingID(clothingID: string): void {
+    this.clothingID = clothingID;
+  }
+
+  /*
+  get clothingName
+  */
+  getClothingName(): string {
+    return this.clothingName;
+  }
+
+  /*
+  set clothingName
+  */
+  setClothingName(clothingName: string): void{
+    return this.clothingName;
+  }
+
+  /*
+  get clothingWorn (number of times clothing item has been worn)
+  */
+  getClothingWorn(): string {
+    return this.clothingWorn;
+  }
+
+  /*
+  set clothingWorn (number of times clothing item has been worn)
+  */
+  setClothingWorn(clothingWorn: string): void{
+    return this.clothingWorn;
+  }
+
+  /*
+  get clothingCost
+  */
+  getClothingCost(): string {
+    return this.clothingCost;
+  }
+
+  /*
+  set clothingCost
+  */
+  setClothingCost(clothingCost: string): void{
+    return this.clothingCost;
+  }
+
+  /*
+  get clothingCategory
+  */
+  getClothingCategory(): string {
+    return this.clothingCategory;
+  }
+
+  /*
+  set clothingCategory
+  */
+  setClothingCategory(clothingCategory: string): void{
+    return this.clothingCategory;
+  }
+
+  /*
+  get clothingPurchaseDate
+  */
+  getClothingPurchaseDate(): string {
+    return this.clothingPurchaseDate;
+  }
+
+  /*
+  set clothingPurchaseDate
+  */
+  setClothingPurchaseDate(clothingPurchaseDate: string): void{
+    return this.clothingPurchaseDate;
+  }
+
+}
