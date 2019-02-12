698e65a897a41d9e93ba701adf2ffe2dfb280156
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-12 13:52:58 -0500

    S-35: Implemented toggleEditMode in profile component.

--- closetr-web/src/app/profile/profile.component.ts
+++ closetr-web/src/app/profile/profile.component.ts
@@ -8,6 +8,8 @@ import { RouterModule, Routes, Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   user: any;
+  editMode: boolean;
+
   constructor(private router: Router) {
     this.user = {
       'userID':'fideslinga',
@@ -15,6 +17,7 @@ export class ProfileComponent implements OnInit {
       'userPassword': 'password',
       'userDesc': 'A girl that is addicted to clothes.'
     };
+    this.editMode = false;
   }
 
   checkSubmit(): boolean {
@@ -26,6 +29,13 @@ export class ProfileComponent implements OnInit {
   */
   back(): void {
     this.router.navigate(['/dashboard']);
+  }
+
+  /*
+  Toggle edit mode
+  */
+  toggleEditMode(): void {
+    this.editMode = !this.editMode;
   }
 
   ngOnInit() {
