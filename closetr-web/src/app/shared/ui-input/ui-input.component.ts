8bc26ed4b5176891b431a335d05bea2e2e5fcab9
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-13 13:30:09 -0500

    S-37: Added types to ui-input, and integrated it to add-clothing component.

--- closetr-web/src/app/shared/ui-input/ui-input.component.ts
+++ closetr-web/src/app/shared/ui-input/ui-input.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-ui-input',
+  selector: 'ui-input',
   templateUrl: './ui-input.component.html',
   styleUrls: ['./ui-input.component.scss']
 })
 export class UiInputComponent implements OnInit {
   @Input() labelText: string;
-  @Input() inputModel: string;
-  @Input() label: string;
+  @Input() type: string;
+  @Input() inputModel: any;
   @Input() placeholder: string;
-  @Output() inputModelChange: EventEmitter<string> = new EventEmitter<string>();
-  @Output() removeCardEmit: EventEmitter<Object> = new EventEmitter<Object>();
+  @Output() inputModelChange: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
