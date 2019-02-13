05a8899158d61bc268b4721e2f519752b1089ba0
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-13 15:27:08 -0500

    S-37: Added styling for ui-text-button, integrated it into closet-manage component.

--- closetr-web/src/app/shared/ui-text-button/ui-text-button.component.ts
+++ closetr-web/src/app/shared/ui-text-button/ui-text-button.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-ui-text-button',
+  selector: 'ui-text-button',
   templateUrl: './ui-text-button.component.html',
   styleUrls: ['./ui-text-button.component.scss']
 })
 export class UiTextButtonComponent implements OnInit {
+  @Input() type: string;
+  @Input() labelText: string;
+  @Input() buttonLink: string;
+  @Output() click: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  buttonClick(): void {
+    this.click.emit();
   }
 
 }
