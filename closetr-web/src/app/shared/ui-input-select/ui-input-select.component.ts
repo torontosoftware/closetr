4962a98d113b20060963ef81eacc98c99cb08ec3
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-13 13:47:35 -0500

    S-37: Added styles to ui-input-select, integrated it to add-clothing component.

--- closetr-web/src/app/shared/ui-input-select/ui-input-select.component.ts
+++ closetr-web/src/app/shared/ui-input-select/ui-input-select.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-ui-input-select',
+  selector: 'ui-input-select',
   templateUrl: './ui-input-select.component.html',
   styleUrls: ['./ui-input-select.component.scss']
 })
 export class UiInputSelectComponent implements OnInit {
+  @Input() labelText: string;
+  @Input() inputModel: any;
+  @Output() inputModelChange: EventEmitter<any> = new EventEmitter<any>();
+  @Input() items: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  modelChange(): void {
+    this.inputModelChange.emit(this.inputModel);
+  }
 }
