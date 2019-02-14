61dc78de202e921284d5c8283d69713f151c553f
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-14 11:41:47 -0500

    S-38: Added logic and styling to ui-select-add-button, used it to replace add budget selector in budget manage component.

--- closetr-web/src/app/shared/ui-select-add-button/ui-select-add-button.component.ts
+++ closetr-web/src/app/shared/ui-select-add-button/ui-select-add-button.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-ui-select-add-button',
+  selector: 'ui-select-add-button',
   templateUrl: './ui-select-add-button.component.html',
   styleUrls: ['./ui-select-add-button.component.scss']
 })
 export class UiSelectAddButtonComponent implements OnInit {
+  @Input() addon: string;
+  @Input() inputModel: any;
+  @Input() placeholder: string;
+  @Output() inputModelChange: EventEmitter<any> = new EventEmitter<any>();
+  @Input() errorMessage: string;
+  @Input() showError: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() addonDisabled: boolean = false;
+  @Output() addonClick: EventEmitter<any> = new EventEmitter<any>();
+  @Input() items: Array<any>;
 
   constructor() { }
 
