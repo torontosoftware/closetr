378a96bb3703ad77c8520fa4e00d59e1c46c715f
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-14 11:07:34 -0500

    S-38: Added logic and styling to ui-input-add-button, used it to replace search bar in closet-manage component.

--- closetr-web/src/app/shared/ui-input-add-button/ui-input-add-button.component.ts
+++ closetr-web/src/app/shared/ui-input-add-button/ui-input-add-button.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-ui-input-add-button',
+  selector: 'ui-input-add-button',
   templateUrl: './ui-input-add-button.component.html',
   styleUrls: ['./ui-input-add-button.component.scss']
 })
 export class UiInputAddButtonComponent implements OnInit {
+  @Input() type: string = 'text';
+  @Input() addon: string;
+  @Input() inputModel: any;
+  @Input() placeholder: string;
+  @Output() inputModelChange: EventEmitter<any> = new EventEmitter<any>();
+  @Input() errorMessage: string;
+  @Input() showError: boolean = false;
+  @Input() disabled: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  modelChange(inputModel): void {
+    this.inputModelChange.emit(inputModel);
+  }
 }
