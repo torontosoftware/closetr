bb727f1e9a56376c861d3cd4d8ee021fd28f2750
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-14 11:56:32 -0500

    S-38: Added logic/styling to ui-input-add-text, integrated it to budget-manage component.

--- closetr-web/src/app/shared/ui-input-add-text/ui-input-add-text.component.ts
+++ closetr-web/src/app/shared/ui-input-add-text/ui-input-add-text.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-ui-input-add-text',
+  selector: 'ui-input-add-text',
   templateUrl: './ui-input-add-text.component.html',
   styleUrls: ['./ui-input-add-text.component.scss']
 })
 export class UiInputAddTextComponent implements OnInit {
+  @Input() type: string = "text";
+  @Input() inputModel: any;
+  @Input() placeholder: string;
+  @Output() inputModelChange: EventEmitter<any> = new EventEmitter<any>();
+  @Input() errorMessage: string;
+  @Input() showError: boolean;
+  @Input() disabled: boolean;
+  @Input() addon: string;
 
   constructor() { }
 
