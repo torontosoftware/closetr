9851c602a72f7459fd3814dd0931e0527fcf1e8b
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-14 16:33:20 -0500

    S-39: Added styling/logic to ui-edit-button, and  added it to closet-manage component.

--- closetr-web/src/app/shared/ui-edit-button/ui-edit-button.component.ts
+++ closetr-web/src/app/shared/ui-edit-button/ui-edit-button.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-ui-edit-button',
+  selector: 'ui-edit-button',
   templateUrl: './ui-edit-button.component.html',
   styleUrls: ['./ui-edit-button.component.scss']
 })
 export class UiEditButtonComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
-
 }
