20b57b90af3f7dde6e7dc49189d659a09d6d231d
desslinga <fides.linga@mail.utoronto.ca>
2019-02-17 21:01:34 -0500

    S-40: Small test file error

--- closetr-web/src/app/pipes/date-range-filter.pipe.spec.ts
+++ closetr-web/src/app/pipes/date-range-filter.pipe.spec.ts
@@ -2,7 +2,7 @@ import { DateRangeFilterPipe } from './date-range-filter.pipe';
 
 describe('DateRangeFilterPipe', () => {
   it('create an instance', () => {
-    const pipe = new DateRangeFilterPipe();
+    const pipe = new DateRangeFilterPipe(true, true, true);
     expect(pipe).toBeTruthy();
   });
 });
