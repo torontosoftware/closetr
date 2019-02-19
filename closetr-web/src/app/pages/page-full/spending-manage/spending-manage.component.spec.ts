91ac6763a01855e446fcf4250728870c73117605
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-19 14:47:25 -0500

    S-35: Fixed spec failures in spending-manage component.

--- closetr-web/src/app/pages/page-full/spending-manage/spending-manage.component.spec.ts
+++ closetr-web/src/app/pages/page-full/spending-manage/spending-manage.component.spec.ts
@@ -1,14 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { UiBackButtonComponent } from '../../../shared/ui-back-button/ui-back-button.component';
+import { UiInputAddButtonComponent } from '../../../shared/ui-input-add-button/ui-input-add-button.component';
+import { UiTextButtonComponent } from '../../../shared/ui-text-button/ui-text-button.component';
+import { UiFilterSelectComponent } from '../../../shared/ui-filter-select/ui-filter-select.component';
 import { SpendingManageComponent } from './spending-manage.component';
+import { DateRangeFilterPipe } from '../../../pipes/date-range-filter.pipe';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'dateRangeFilter'})
+class DateRangeFilterPipeMock implements PipeTransform{
+ transform(items: any, dateFrom: Date, dateTo: Date, property: string) {
+  return items;
+ }
+}
 
 describe('SpendingManageComponent', () => {
   let component: SpendingManageComponent;
   let fixture: ComponentFixture<SpendingManageComponent>;
 
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SpendingManageComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FormsModule
+      ],
+      declarations: [
+        UiBackButtonComponent,
+        UiInputAddButtonComponent,
+        UiTextButtonComponent,
+        UiFilterSelectComponent,
+        SpendingManageComponent,
+        DateRangeFilterPipeMock
+      ],
+      providers: [
+        {provide: DateRangeFilterPipe, useClass: DateRangeFilterPipeMock}
+      ]
     })
     .compileComponents();
   }));
