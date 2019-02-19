5b0f3ca58bb9a73bda20ab425e19f94a2c7b8c95
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-19 16:34:54 -0500

    S-35: Fixed spec failures in budget-manage component.

--- closetr-web/src/app/pages/page-full/budget-manage/budget-manage.component.spec.ts
+++ closetr-web/src/app/pages/page-full/budget-manage/budget-manage.component.spec.ts
@@ -1,5 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UiBackButtonComponent } from '../../../shared/ui-back-button/ui-back-button.component';
+import { UiEditButtonComponent } from '../../../shared/ui-edit-button/ui-edit-button.component';
+import { UiTextButtonComponent } from '../../../shared/ui-text-button/ui-text-button.component';
+import { UiSelectAddButtonComponent } from '../../../shared/ui-select-add-button/ui-select-add-button.component';
+import { UiInputAddTextComponent } from '../../../shared/ui-input-add-text/ui-input-add-text.component';
 import { BudgetManageComponent } from './budget-manage.component';
 
 describe('BudgetManageComponent', () => {
@@ -8,7 +14,18 @@ describe('BudgetManageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BudgetManageComponent ]
+      imports: [
+        FormsModule,
+        RouterTestingModule
+      ],
+      declarations: [
+        UiBackButtonComponent,
+        UiEditButtonComponent,
+        UiTextButtonComponent,
+        UiSelectAddButtonComponent,
+        UiInputAddTextComponent,
+        BudgetManageComponent
+      ]
     })
     .compileComponents();
   }));
