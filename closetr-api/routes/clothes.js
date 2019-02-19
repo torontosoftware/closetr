59aede1105e6286b8bb755239af8cf1761bc5d9e
Joshua Concon <joshua.concon@scotiabank.com>
2019-02-19 16:13:04 -0500

    S-44: config file refactor and cleaning up lambda functions in clothes route

--- closetr-api/routes/clothes.js
+++ closetr-api/routes/clothes.js
@@ -5,9 +5,15 @@ const mongoose = require('mongoose');
 const clothes = require('../models/clothes.model');
 
 /* API sets one new user clothing */
-router.post('/clothing', function(req, res, next) {
+router.post('/clothing', add_new_clothing);
+/* API deletes one clothing item by id. */
+router.delete('/clothing/:clothing_id', delete_clothing);
+/* API returns all user clothes */
+router.get('/all', get_all_user_clothing);
+
+function add_new_clothing(req, res, next) {
   // gather attributes from request
-  var clothing = req.body.clothing;
+  const clothing = req.body.clothing;
   const newItem = {
     clothingName: clothing.clothingName,
     clothingCost: clothing.clothingCost,
@@ -28,84 +34,71 @@ router.post('/clothing', function(req, res, next) {
     {_id: newItem._id},
     newItem,
     {upsert: true, new: true, runValidators: true},
-    function (err, doc) {
-      if (err) {
-        const result_json = {
-          status: 'failed',
-          message: err.message
-        };
-        res.json(result_json);
-      } else {
-        const result_json = {
-          status: 'success',
-          data: doc
-        };
-        res.json(result_json);
-      }
-    }
+    generic_error_handling
   );
-});
+}
 
-
-/* API returns all user clothes */
-router.get('/all', function(req, res, next) {
-  // query all clothes in the database
-  var userID = req.query.userID;
-  clothes.find(
-    {userID: userID},
-    function (err, doc) {
-      if (err) {
-        const result_json = {
-          status: 'failed',
-          message: err.message
-        };
-        res.json(result_json);
-      } else {
-        var result = [];
-        doc.forEach(function(clothing) {
-          var clothingResult = {
-            clothingID: clothing._id,
-            clothingName: clothing.clothingName,
-            clothingCategory: clothing.clothingCategory,
-            clothingWorn: clothing.clothingWorn,
-            clothingCost: clothing.clothingCost,
-            clothingPurchaseDate: clothing.clothingPurchaseDate
-          }
-          result.push(clothingResult);
-        });
-        const result_json = {
-          status: 'success',
-          data: result
-        };
-        res.json(result_json);
-      }
-    }
-  );
-});
-
-/* API deletes one clothing item by id. */
-router.delete('/clothing/:clothing_id', function(req, res, next) {
+function delete_clothing(req, res, next) {
   // gather attributes from request
-  var clothingID = req.params.clothing_id;
+  const clothingID = req.params.clothing_id;
   // create new clothing from clothes schema
   clothes.remove(
     {_id: clothingID},
-    function (err, doc) {
-      if (err) {
-        const result_json = {
-          status: 'failed',
-          message: err.message,
-        };
-        res.json(result_json);
-      } else {
-        const result_json = {
-          status: 'success',
-          data: doc
-        };
-        res.json(result_json);
-      }
-    }
+    generic_error_handling
   );
-});
+}
+
+function get_all_user_clothing(req, res, next) {
+  // query all clothes in the database
+  const userID = req.query.userID;
+  clothes.find(
+    {userID: userID},
+    get_all_clothing_doc_handler
+  );
+}
+
+function get_all_clothing_error_handling(err, doc) {
+  if (err) {
+    const result_json = {
+      status: 'failed',
+      message: err.message
+    };
+    res.json(result_json);
+  } else {
+    var result = [];
+    doc.forEach(function(clothing) {
+      var clothingResult = {
+        clothingID: clothing._id,
+        clothingName: clothing.clothingName,
+        clothingCategory: clothing.clothingCategory,
+        clothingWorn: clothing.clothingWorn,
+        clothingCost: clothing.clothingCost,
+        clothingPurchaseDate: clothing.clothingPurchaseDate
+      }
+      result.push(clothingResult);
+    });
+    const result_json = {
+      status: 'success',
+      data: result
+    };
+    res.json(result_json);
+  }
+}
+
+function generic_error_handling(err, doc) {
+  if (err) {
+    const result_json = {
+      status: 'failed',
+      message: err.message,
+    };
+    res.json(result_json);
+  } else {
+    const result_json = {
+      status: 'success',
+      data: doc
+    };
+    res.json(result_json);
+  }
+}
 
 module.exports = router;
