abce42995141467cecd3b186d7c6481b9095d0cd
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-19 15:02:02 -0500

    S-35: Fixed spec failures in login component.

--- closetr-web/src/app/pages/page-full/login/login.component.spec.ts
+++ closetr-web/src/app/pages/page-full/login/login.component.spec.ts
@@ -1,5 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UiInputComponent } from '../../../shared/ui-input/ui-input.component';
+import { UiTextButtonComponent } from '../../../shared/ui-text-button/ui-text-button.component';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,7 +12,16 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [
+        FormsModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [
+        UiInputComponent,
+        UiTextButtonComponent,
+        LoginComponent
+      ]
     })
     .compileComponents();
   }));
