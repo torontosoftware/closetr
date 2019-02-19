d6ca05d3e58dd8d39edbe846b47564e3779d9be2
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-19 14:54:11 -0500

    S-35: Fixed spec failures in register component.

--- closetr-web/src/app/pages/page-full/register/register.component.spec.ts
+++ closetr-web/src/app/pages/page-full/register/register.component.spec.ts
@@ -1,5 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UiInputComponent } from '../../../shared/ui-input/ui-input.component';
+import { UiTextButtonComponent } from '../../../shared/ui-text-button/ui-text-button.component';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
@@ -8,7 +12,16 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FormsModule
+      ],
+      declarations: [
+        UiInputComponent,
+        UiTextButtonComponent,
+        RegisterComponent
+      ]
     })
     .compileComponents();
   }));
