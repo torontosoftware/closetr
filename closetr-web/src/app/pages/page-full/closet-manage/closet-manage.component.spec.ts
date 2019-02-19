d89e5f2a8118c301bc006bda747ae8f9926b58a6
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-19 16:24:03 -0500

    S-35: Fixed spec failures in closet manage component.

--- closetr-web/src/app/pages/page-full/closet-manage/closet-manage.component.spec.ts
+++ closetr-web/src/app/pages/page-full/closet-manage/closet-manage.component.spec.ts
@@ -1,6 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { SearchFilterPipe } from '../../../pipes/search-filter.pipe';
+import { UiBackButtonComponent } from '../../../shared/ui-back-button/ui-back-button.component';
+import { UiEditButtonComponent } from '../../../shared/ui-edit-button/ui-edit-button.component';
+import { UiTextButtonComponent } from '../../../shared/ui-text-button/ui-text-button.component';
+import { UiInputAddButtonComponent } from '../../../shared/ui-input-add-button/ui-input-add-button.component';
+import { UiFilterSelectComponent } from '../../../shared/ui-filter-select/ui-filter-select.component';
+import { UiCloseButtonComponent } from '../../../shared/ui-close-button/ui-close-button.component';
+import { ClosetCardComponent } from '../../page-partial/closet-card/closet-card.component';
 import { ClosetManageComponent } from './closet-manage.component';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'filter'})
+class SearchFilterPipeMock implements PipeTransform{
+  transform(items: any, searchText: String, property: string) {
+    return ;
+  }
+}
 
 describe('ClosetManageComponent', () => {
   let component: ClosetManageComponent;
@@ -8,7 +26,25 @@ describe('ClosetManageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ClosetManageComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FormsModule
+      ],
+      declarations: [
+        UiBackButtonComponent,
+        UiEditButtonComponent,
+        UiTextButtonComponent,
+        UiInputAddButtonComponent,
+        UiFilterSelectComponent,
+        UiCloseButtonComponent,
+        ClosetCardComponent,
+        ClosetManageComponent,
+        SearchFilterPipeMock
+      ],
+      providers: [
+        {provide: SearchFilterPipe, useClass: SearchFilterPipeMock}
+      ]
     })
     .compileComponents();
   }));
