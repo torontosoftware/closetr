326aefbff4d070ffae82cb16a5b119d164a8386d
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-19 11:54:48 -0500

    S-35: Attempting to make mock objects.

--- closetr-web/src/app/pages/page-partial/today-widget/today-widget.component.spec.ts
+++ closetr-web/src/app/pages/page-partial/today-widget/today-widget.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthenticationService } from '../../../services/authentication.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { UiTextButtonComponent } from '../../../shared/ui-text-button/ui-text-button.component';
 import { TodayWidgetComponent } from './today-widget.component';
@@ -8,8 +9,14 @@ import { User } from '../../../models/user.model';
 describe('TodayWidgetComponent', () => {
   let component: TodayWidgetComponent;
   let fixture: ComponentFixture<TodayWidgetComponent>;
+  let mockUser: any;
+  let currentUser: any;
 
   beforeEach(async(() => {
+    mockUser = {
+      userName: "Fides",
+      userID: "fideslinga"
+    };
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
@@ -18,18 +25,23 @@ describe('TodayWidgetComponent', () => {
       declarations: [
         UiTextButtonComponent,
         TodayWidgetComponent
+      ],
+      providers: [
+        {provide: User, useValue: mockUser},
+        AuthenticationService
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodayWidgetComponent);
     component = fixture.componentInstance;
+    currentUser = TestBed.get(User);
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    currentUser.userName = "Fides";
     expect(component).toBeTruthy();
   });
 });
