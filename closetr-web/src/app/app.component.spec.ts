8845a69ba5b81de75238426ecfb38f224170d3ef
desslinga <fides.linga@mail.utoronto.ca>
2019-02-18 23:13:28 -0500

    S-35: Attempting to fix more errors

--- closetr-web/src/app/app.component.spec.ts
+++ closetr-web/src/app/app.component.spec.ts
@@ -3,12 +3,17 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { UserMenuComponent } from './pages/page-partial/user-menu/user-menu.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { fadeAnimation } from './animations/animations';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        BrowserAnimationsModule
       ],
       declarations: [
         AppComponent,
@@ -30,10 +35,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('closeter');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to closeter!');
-  });
 });
--- closetr-web/src/app/guards/auth.guard.spec.ts
+++ closetr-web/src/app/guards/auth.guard.spec.ts
@@ -1,15 +1,31 @@
 import { TestBed, async, inject } from '@angular/core/testing';
-
 import { AuthGuard } from './auth.guard';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+class MockRouter {
+  navigate = jasmine.createSpy('navigate');
+}
 
 describe('AuthGuard', () => {
   beforeEach(() => {
+    const mockRouter = new MockRouter();
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      providers: [
+        {provide: Router, useClass: MockRouter},
+        AuthGuard,
+        AuthenticationService,
+        HttpClientTestingModule
+      ]
     });
   });
 
-  it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
+  it('should ...', inject([
+    AuthGuard,
+    AuthenticationService,
+    Router],
+    (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
   }));
 });
