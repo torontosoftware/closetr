2a8952b7db6bb444aa7475c8d9112c7cf73a485f
desslinga <fides.linga@mail.utoronto.ca>
2019-02-26 23:13:40 -0500

    S-45: Minor changes.

--- closetr-web/src/app/pages/page-full/login/login.component.spec.ts
+++ closetr-web/src/app/pages/page-full/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed} from '@angular/core/testing';
 import { Injectable, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthenticationService } from '../../../services/authentication.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -25,9 +26,12 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let authenticationService: AuthenticationServiceMock;
   let fixture: ComponentFixture<LoginComponent>;
+  let location: Location;
+  let router: Router;
   const routes = [
     { path: 'dashboard', component: MockDashboardComponent }
   ];
+  const routerSpy = jasmine.createSpyObj('Router',['navigateByUrl']);
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -52,6 +56,8 @@ describe('LoginComponent', () => {
     console.log("howdy", component);
     //component = TestBed.get(LoginComponent);
     authenticationService = TestBed.get(AuthenticationService);
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
   });
   /*
   beforeEach(() => {
@@ -66,27 +72,24 @@ describe('LoginComponent', () => {
   });*/
 
   it('should redirect to dashboard if logged in', () => {
+
+    console.log("location",location);
     console.log("starting first case", component);
-    authenticationService.currentUserValue = "yes";
     fixture.detectChanges();
+    console.log(component.router,component.router.url);
+    expect(component.router.url).toEqual('/dashboard');
     //expect(component.router.url).toEqual('/dashboard');
   });
 
   it('should not redirect to dashboard if not logged in', () => {
     console.log("starting second case");
-    authenticationService.currentUserValue = 0;
-    console.log(component,"should not redirect to dashboard");
+
+    console.log("locatoin",location);
+    authenticationService.currentUserValue = null;
     fixture.detectChanges();
-    expect(component).toBeTruthy();
+    console.log(component.router,component.router.url);
+    expect(component.router.url).toEqual('/');
   });
 
-  it('should redirect to dashboard', () => {
-    console.log("starting third case",component);
-    authenticationService.currentUserValue = "pls";
-    console.log(component,"third: should not redirect to dashboard");
-    fixture.detectChanges();
-    //fixture.detectChanges();
-    expect(component).toBeTruthy();
-  });
 
 });
