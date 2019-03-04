29317f9df7dc56c1f15cc254d304c0f174abd8ef
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-04 10:39:38 -0500

    S-47: Importing some modules.

--- closetr-web/src/app/pages/page-full/register/register.component.spec.ts
+++ closetr-web/src/app/pages/page-full/register/register.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Injectable, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
