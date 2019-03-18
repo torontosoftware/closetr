14cd11bf5abcee1ef371a6eb0f2c6573ede7753a
desslinga <fides.linga@mail.utoronto.ca>
2019-03-17 23:48:33 -0400

    S-58: Initial setup and scaffolding of init tests.

--- closetr-web/src/app/pages/page-full/profile/profile.component.spec.ts
+++ closetr-web/src/app/pages/page-full/profile/profile.component.spec.ts
@@ -1,30 +1,87 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Injectable } from '@angular/core';
+import { of } from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { UserService } from '../../../services/user.service';
 import { UiBackButtonComponent } from '../../../shared/ui-back-button/ui-back-button.component';
 import { UiEditButtonComponent } from '../../../shared/ui-edit-button/ui-edit-button.component';
 import { UiTextButtonComponent } from '../../../shared/ui-text-button/ui-text-button.component';
 import { UiInputComponent } from '../../../shared/ui-input/ui-input.component';
+import { User } from '../../../models/user.model';
 import { ProfileComponent } from './profile.component';
+
+const currentUser = new User({
+  userID: 'fideslinga',
+  userName: 'Fides Linga',
+  userDesc: 'description',
+  userPassword: 'password'
+});
+
+const updatedUser = new User({
+  userID: 'fideslinga',
+  userName: 'Fidessa Linga',
+  userDesc: 'a big chungus',
+  userPassword: 'password'
+});
+
+@Component({
+  selector: 'app-dashboard',
+  template: '<p>Mock Dashboard Component'
+})
+class MockDashboardComponent { }
+
+@Injectable({
+  providedIn: 'root'
+})
+class AuthenticationServiceMock {
+  currentUser = of(currentUser);
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+class UserServiceMock {
+  update = (user) => of({data: updatedUser});
+}
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
+  let hostElement;
+  let saveButton;
+  let editButton;
+  let usernameInput: HTMLInputElement;
+  let nameInput: HTMLInputElement;
+  let descriptionInput: HTMLInputElement;
+  let passwordInput: HTMLInputElement;
+
+  const routes = [
+    { path: 'dashboard', component: MockDashboardComponent }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         FormsModule,
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(routes),
         HttpClientTestingModule
       ],
       declarations: [
+        MockDashboardComponent,
         UiBackButtonComponent,
         UiEditButtonComponent,
         UiTextButtonComponent,
         UiInputComponent,
         ProfileComponent
+      ],
+      providers: [
+        ProfileComponent,
+        { provide: UserService, useClass: UserServiceMock },
+        { provide: AuthenticationService, useClass: AuthenticationServiceMock }
       ]
     })
     .compileComponents();
@@ -33,10 +90,34 @@ describe('ProfileComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
+    hostElement = fixture.nativeElement;
     fixture.detectChanges();
+    saveButton = hostElement.querySelector('#save-button button');
+    editButton = hostElement.querySelector('#edit-button button');
+    usernameInput = hostElement.querySelector('#username-input input');
+    nameInput = hostElement.querySelector('#name-input input');
+    descriptionInput = hostElement.querySelector('#description-input input');
+    passwordInput = hostElement.querySelector('#password-input input');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe(`from the init method`, () => {
+    it(`should retrieve the current user from
+      authentication service.`, () => {
+
+    });
+    it(`should have editMode as false initially.`, () => {
+
+    });
+    it(`should populate the form fields properly.`, () => {
+
+    });
+    it(`should have all fields disabled.`, () => {
+
+    });
+  });
+
 });
