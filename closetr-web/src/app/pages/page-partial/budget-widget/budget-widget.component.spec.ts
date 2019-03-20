1872674699612259ddce7c57c469a2aabd251c64
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-20 14:11:53 -0400

    S-60: Added mock services and objects.

--- closetr-web/src/app/pages/page-partial/budget-widget/budget-widget.component.spec.ts
+++ closetr-web/src/app/pages/page-partial/budget-widget/budget-widget.component.spec.ts
@@ -1,8 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Pipe, PipeTransform } from '@angular/core';
+import { of } from 'rxjs';
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { ClosetService } from '../../../services/closet.service';
+import { User } from '../../../models/user.model';
+import { Clothing } from '../../../models/clothing.model';
 import { DateRangeFilterPipe } from '../../../pipes/date-range-filter.pipe';
 import { UiTableComponent } from '../../../shared/ui-table/ui-table.component';
 import { UiFilterSelectComponent } from '../../../shared/ui-filter-select/ui-filter-select.component';
@@ -10,11 +15,33 @@ import { UiTextButtonComponent } from '../../../shared/ui-text-button/ui-text-bu
 import { UiEditButtonComponent } from '../../../shared/ui-edit-button/ui-edit-button.component';
 import { BudgetWidgetComponent } from './budget-widget.component';
 
+const closetList = [
+  new Clothing({clothingID: '1', clothingName: 'tshirt'}),
+  new Clothing({clothingID: '2', clothingName: 'jeans'}),
+  new Clothing({clothingID: '3', clothingName: 'shoes'})
+];
+
+const currentUser = new User({userName: 'fides', id: '1'});
+
 @Pipe({name: 'dateRangeFilter'})
 class DateRangeFilterPipeMock implements PipeTransform {
   transform(items: any, dateFrom: Date, dateTo: Date, property: string) {
    return items;
   }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+class AuthenticationServiceMock {
+  currentUser = of(currentUser);
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+class ClosetServiceMock {
+  getAllClothes = (user) => of({data: closetList});
 }
 
 describe('BudgetWidgetComponent', () => {
@@ -38,7 +65,9 @@ describe('BudgetWidgetComponent', () => {
         BudgetWidgetComponent
       ],
       providers: [
-        {provide: DateRangeFilterPipe, useClass: DateRangeFilterPipeMock}
+        { provide: AuthenticationService, useClass: AuthenticationServiceMock },
+        { provide: ClosetService, useClass: ClosetServiceMock },
+        { provide: DateRangeFilterPipe, useClass: DateRangeFilterPipeMock }
       ]
     })
     .compileComponents();
