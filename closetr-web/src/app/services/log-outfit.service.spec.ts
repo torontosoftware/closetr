62edaeb226e3e0168f42f5dfbb3aeb89a5cb229b
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-25 11:47:27 -0400

    S-65: Scaffolded all tests.

--- closetr-web/src/app/services/log-outfit.service.spec.ts
+++ closetr-web/src/app/services/log-outfit.service.spec.ts
@@ -11,4 +11,36 @@ describe('LogOutfitService', () => {
     const service: LogOutfitService = TestBed.get(LogOutfitService);
     expect(service).toBeTruthy();
   });
+
+  describe(`calling addOutfitClothing(),`, () => {
+    it(`should make a POST request to base url
+      with given params.`, () => {
+
+    });
+    it(`should return the observable result.`, () => {
+
+    });
+  });
+  describe(`calling deleteOutfitClothing(),`, () => {
+    it(`should make a DELETE request to base url
+      with given params.`, () => {
+
+    });
+    it(`should return the observable result.`, () => {
+
+    });
+  });
+  describe(`calling getAllOutfitClothes(),`, () => {
+    it(`should create HttpParams with the
+      given params.`, () => {
+
+    });
+    it(`should make a GET request to base
+      url with converted params.`, () => {
+
+    });
+    it(`should return the observable result.`, () => {
+
+    });
+  });
 });
