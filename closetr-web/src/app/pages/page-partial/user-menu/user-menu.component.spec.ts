e5e01df8a0566db12e43e4e600d0c1b577f7ed34
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-25 10:18:38 -0400

    S-63: Scaffolded all tests.

--- closetr-web/src/app/pages/page-partial/user-menu/user-menu.component.spec.ts
+++ closetr-web/src/app/pages/page-partial/user-menu/user-menu.component.spec.ts
@@ -34,4 +34,59 @@ describe('UserMenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe(`the ui-popup-menu-item`, () => {
+    it(`should render from my-profile link.`, () => {
+
+    });
+    it(`should render from settings link.`, () => {
+
+    });
+    it(`should render from sign-out link.`, () => {
+
+    });
+  });
+
+  describe(`the navClick() function,`, () => {
+    it(`should be called with '/profile' when
+      the profile link button is clicked.`, () => {
+
+    });
+    it(`should be called with '/settings' when
+      the settings link button is clicked.`, () => {
+
+    });
+    it(`should be called with '/sign-out' when
+      the sign-out link button is clicked.`, () => {
+
+    });
+    describe(`when called with a link,`, () => {
+      it(`should navigate to /profile and call close(),
+        when it is called with /profile.`, () => {
+
+      });
+      it(`should call logout() and then close(),
+        when it is called with /sign-out.`, () => {
+
+      });
+    });
+  });
+
+  describe(`the logout() function,`, () => {
+    it(`should call authentication service's
+      logout function.`, () => {
+
+    });
+    it(`should navigate to login page.`, () => {
+
+    });
+  });
+
+  describe(`the close() function`, () => {
+    it(`should call closeUserMenuEmit's
+      emit function,` () => {
+
+    });
+  });
+
 });
