5a87baa5d36376f88735deecf4689c0734de5265
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-25 11:03:29 -0400

    S-63: Defined cases for navClick(), for it's own functionality on diff inputs.

--- closetr-web/src/app/pages/page-partial/user-menu/user-menu.component.spec.ts
+++ closetr-web/src/app/pages/page-partial/user-menu/user-menu.component.spec.ts
@@ -1,23 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UiPopupMenuItemComponent } from '../../../shared/ui-popup-menu-item/ui-popup-menu-item.component';
 import { UserMenuComponent } from './user-menu.component';
 
+@Component({
+  selector: 'app-profile',
+  template: '<p> Mock Profile Component </p>'
+})
+class MockProfileComponent { }
+
+@Component({
+  selector: 'app-login',
+  template: '<p> Mock Login Component </p>'
+})
+class MockLoginComponent { }
+
 describe('UserMenuComponent', () => {
   let component: UserMenuComponent;
   let fixture: ComponentFixture<UserMenuComponent>;
+  let router: Router;
   let hostElement;
+
+  const routes = [
+    { path: 'profile', component: MockProfileComponent },
+    { path: 'login', component: MockLoginComponent }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
+        MockProfileComponent,
+        MockLoginComponent,
         UiPopupMenuItemComponent,
         UserMenuComponent
       ],
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(routes),
         HttpClientTestingModule
       ],
       providers: [
@@ -30,8 +52,12 @@ describe('UserMenuComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserMenuComponent);
     component = fixture.debugElement.componentInstance;
+    router = TestBed.get(Router);
     hostElement = fixture.nativeElement;
-    spyOn(component, 'navClick');
+    spyOn(component, 'navClick').and.callThrough();
+    spyOn(component, 'close').and.callThrough();
+    spyOn(component, 'logout');
+    spyOn(router, 'navigate').and.callThrough();
     fixture.detectChanges();
   });
 
@@ -110,11 +136,15 @@ describe('UserMenuComponent', () => {
   describe(`the navClick() function,`, () => {
     it(`should navigate to /profile and call close(),
       when it is called with /profile.`, () => {
-
+      component.navClick('/profile');
+      expect(router.navigate).toHaveBeenCalledWith(['/profile']);
+      expect(component.close).toHaveBeenCalled();
     });
     it(`should call logout() and then close(),
       when it is called with /sign-out.`, () => {
-
+      component.navClick('/sign-out');
+      expect(component.logout).toHaveBeenCalled();
+      expect(component.close).toHaveBeenCalled();
     });
   });
 
