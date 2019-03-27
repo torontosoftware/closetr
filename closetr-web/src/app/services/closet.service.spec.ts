288bf1c288f323fabf26846093f87a64e7a2d18a
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-27 10:25:10 -0400

    S-64: Scaffolding, and added addClothing() test.

--- closetr-web/src/app/services/closet.service.spec.ts
+++ closetr-web/src/app/services/closet.service.spec.ts
@@ -1,14 +1,60 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Clothing } from '../models/clothing.model';
 import { ClosetService } from './closet.service';
 
 describe('ClosetService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule ]
-  }));
+  let httpTestingController: HttpTestingController;
+  let closetService: ClosetService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ ClosetService ]
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
+    closetService = TestBed.get(ClosetService);
+  });
 
   it('should be created', () => {
-    const service: ClosetService = TestBed.get(ClosetService);
-    expect(service).toBeTruthy();
+    expect(closetService).toBeTruthy();
+  });
+
+  describe(`calling addClothing()`, () => {
+    it(`should make a POST request to base url with
+      given params, and return correct data.`, () => {
+      const newClothingID = "clothingID";
+      const newClothing = new Clothing({
+        clothingName: 'TShirt',
+        clothingCost: 10,
+        clothingWorn: 10,
+        clothingPurchaseDate: '2019-03-27',
+        clothingCategory: 'Top'
+      });
+      closetService.addClothing(newClothing)
+      .subscribe(data => {
+        let clothing = data.data;
+        expect(clothing._id).toEqual(newClothingID);
+        expect(clothing.clothingWorn).toEqual(newClothing.clothingWorn);
+        expect(clothing.clothingCost).toEqual(newClothing.clothingCost);
+        expect(clothing.clothingPurchaseDate).toEqual(newClothing.clothingPurchaseDate);
+        expect(clothing.clothingCategory).toEqual(newClothing.clothingCategory);
+      });
+      const req = httpTestingController.expectOne(
+        `http://localhost:8080/api/clothes/clothing`
+      );
+      expect(req.request.method).toEqual('POST');
+      const response = {
+        status: 'success',
+        data: {
+          ...newClothing,
+          _id: newClothingID
+        }
+      };
+      req.flush(response);
+    });
   });
 });
