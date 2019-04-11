f64c876a5d813452c3e3051083cfd3d2898b12d2
JoshuaConcon <conconjoshua@gmail.com>
2019-04-11 09:53:33 -0400

    S-69: Saving work to continue on another computer

--- closetr-api/components/clothes/clothes.controller.js
+++ closetr-api/components/clothes/clothes.controller.js
@@ -2,23 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const clothes_model = require('./clothes.model');
 
+/**
+ * @desc Add a new clothing to the database given a clothing object in the
+ * request body
+ */
 function add_new_clothing(req, res, next) {
   // gather attributes from request
   const clothing = req.body.clothing;
-  const newItem = {
-    clothingName: clothing.clothingName,
-    clothingCost: clothing.clothingCost,
-    clothingCategory: clothing.clothingCategory,
-    clothingWorn: clothing.clothingWorn,
-    clothingPurchaseDate: clothing.clothingPurchaseDate,
-    user: clothing.userID
-  };
-
-  if (clothing.clothingID == null) {
-    newItem['_id'] = mongoose.Types.ObjectId();
-  } else {
-    newItem['_id'] = clothing.clothingID;
-  }
+  const newItem = extract_clothing_payload_from_clothing_object(clothing);
 
   // create new clothing from clothes schema
   clothes_model.findOneAndUpdate(
@@ -29,6 +20,9 @@ function add_new_clothing(req, res, next) {
   );
 }
 
+/**
+ * @desc Delete a specific clothing from @code{clothing_id} in the request params
+ */
 function delete_clothing(req, res, next) {
   // gather attributes from request
   const clothingID = req.params.clothing_id;
@@ -48,48 +42,88 @@ function get_all_user_clothing(req, res, next) {
   );
 }
 
-function get_all_clothing_error_handling(err, doc, res) {
-  if (err) {
-    const result_json = {
-      status: 'failed',
-      message: err.message
-    };
-    res.json(result_json);
+function extract_clothing_payload_from_clothing_object(clothing) {
+  let newItem = {
+    clothingName: clothing.clothingName,
+    clothingCost: clothing.clothingCost,
+    clothingCategory: clothing.clothingCategory,
+    clothingWorn: clothing.clothingWorn,
+    clothingPurchaseDate: clothing.clothingPurchaseDate,
+    user: clothing.userID
+  };
+
+  if (clothing.clothingID == null) {
+    newItem['_id'] = mongoose.Types.ObjectId();
   } else {
-    var result = [];
-    doc.forEach(function(clothing) {
-      var clothingResult = {
-        clothingID: clothing._id,
-        clothingName: clothing.clothingName,
-        clothingCategory: clothing.clothingCategory,
-        clothingWorn: clothing.clothingWorn,
-        clothingCost: clothing.clothingCost,
-        clothingPurchaseDate: clothing.clothingPurchaseDate
-      }
-      result.push(clothingResult);
-    });
-    const result_json = {
-      status: 'success',
-      data: result
-    };
-    res.json(result_json);
+    newItem['_id'] = clothing.clothingID;
   }
+
+  return newItem
 }
 
-function generic_error_handling(err, doc, res) {
+function get_all_clothing_error_handling(err, doc, res) {
+  clothing_loading_wrapper = function(){ return get_all_clothing_loading(doc); };
+  success_callback_error_handling(err, clothing_loading_wrapper);
+  res.json(result_json);
+}
+
+function get_all_clothing_loading(doc) {
+  var result = [];
+  doc.forEach(function(clothing) {
+    var clothingResult = {
+      clothingID: clothing._id,
+      clothingName: clothing.clothingName,
+      clothingCategory: clothing.clothingCategory,
+      clothingWorn: clothing.clothingWorn,
+      clothingCost: clothing.clothingCost,
+      clothingPurchaseDate: clothing.clothingPurchaseDate
+    }
+    result.push(clothingResult);
+  });
+  const result_json = generic_success(result);
+
+  return result_json
+}
+
+function generic_error_handling(err, payload, res) {
+  generic_success_wrapper = function(){ return generic_success(payload); };
+  result_json = success_callback_error_handling(err, generic_success_wrapper)
+  res.json(result_json);
+}
+
+function generic_success(payload) {
+  const result_json = {
+    status: 200,
+    data: payload
+  };
+  return result_json
+}
+
+/**
+ *
+ */
+function generic_fail(){
+  const result_json = {
+    status: 500,
+    message: err.message,
+  };
+  return result_json;
+}
+
+/**
+ * @desc Returns the resulting json of a generic fail if there is an error, or
+ * calls a callback_function with no arguments otherwise.
+ * @returns the result as a json
+ * @param err error that determines success or failure
+ * @param callback_function function that is called if no error is found
+ */
+function success_callback_error_handling(err, callback_function) {
   if (err) {
-    const result_json = {
-      status: 'failed',
-      message: err.message,
-    };
-    res.json(result_json);
+    result_json = generic_fail()
   } else {
-    const result_json = {
-      status: 'success',
-      data: doc
-    };
-    res.json(result_json);
+    result_json = callback_function();
   }
+  return result_json
 }
 
 var clothing_module = {
