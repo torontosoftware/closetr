c8ed4b4a1ca6f9ce7299f153e37205c78bbe23e1
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-01 11:03:56 -0400

    S-78: Breaking up checkError function to multiple for each field.

--- closetr-web/src/app/pages/page-full/register/register.component.ts
+++ closetr-web/src/app/pages/page-full/register/register.component.ts
@@ -61,39 +61,52 @@ export class RegisterComponent implements OnInit {
     };
   }
 
-  checkError(): void {
-    this.resetErrors();
+  checkErrorPasswordConfirm(): void {
     if (this.password.length != 0
         && this.passwordConfirm.length != 0
         && this.password != this.passwordConfirm) {
           this.errorMessage.passwordConfirm = 'passwords are not the same.';
           this.error.passwordConfirm = true;
-        }
+     }
+  }
 
+  checkErrorPassword(): void {
     if (this.password.length == 0
         && this.passwordConfirm.length != 0) {
           this.errorMessage.password = 'password is required.'
           this.error.password = true;
-        }
+    }
+  }
 
+  checkErrorUsername(): void {
     if ((this.password.length != 0
         || this.passwordConfirm.length != 0)
         && this.username.length == 0) {
           this.errorMessage.username = 'username is required.'
           this.error.username = true;
-        }
+    }
     if (this.userExists) {
       this.errorMessage.username = 'user with that username already exists.';
       this.error.username = true;
     }
+  }
 
+  checkErrorName(): void {
     if ((this.username.length != 0
          || this.password.length != 0
          || this.passwordConfirm.length != 0)
         && (this.name.length == 0)) {
           this.errorMessage.name = 'name is required.';
           this.error.name = true;
-        }
+    }
+  }
+
+  checkError(): void {
+    this.resetErrors();
+    this.checkErrorPasswordConfirm();
+    this.checkErrorPassword();
+    this.checkErrorUsername();
+    this.checkErrorName();
   }
 
   toSignIn(): void {
