c2d794462e2e358a2df01eb36309e89e5452ffb2
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-02 09:51:32 -0400

    S-79: Scaffolding and finished ui-back-button tests.

--- closetr-web/src/app/shared/ui-back-button/ui-back-button.component.spec.ts
+++ closetr-web/src/app/shared/ui-back-button/ui-back-button.component.spec.ts
@@ -1,26 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { UiBackButtonComponent } from './ui-back-button.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockDashboardComponent } from '../../../test/components';
+import { clickAndTestNavigate } from '../../../test/utils';
 
 describe('UiBackButtonComponent', () => {
   let component: UiBackButtonComponent;
   let fixture: ComponentFixture<UiBackButtonComponent>;
+  let router: Router;
+  let hostElement;
+
+  const routes = [
+    { path: 'dashboard', component: MockDashboardComponent }
+  ];
+
+  const url = '/dashboard';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UiBackButtonComponent ],
-      imports: [ RouterTestingModule ]
+      declarations: [
+        MockDashboardComponent,
+        UiBackButtonComponent
+      ],
+      imports: [ RouterTestingModule.withRoutes(routes) ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UiBackButtonComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
+    hostElement = fixture.nativeElement;
+    router = TestBed.get(Router);
+    spyOn(router, 'navigate');
+    component.url = url;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`clicking back button should navigate to previous url.`, () => {
+    clickAndTestNavigate(
+      hostElement.querySelector('button.btn-back'),
+      router,
+      '/dashboard',
+      fixture
+    );
   });
 });
