a4555d5a919365e0975a3394d47698a8cfe96c3f
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-02 10:40:45 -0400

    S-79: Finished ui-close-button tests.

--- closetr-web/src/app/shared/ui-close-button/ui-close-button.component.spec.ts
+++ closetr-web/src/app/shared/ui-close-button/ui-close-button.component.spec.ts
@@ -1,21 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UiCloseButtonComponent } from './ui-close-button.component';
 
 describe('UiCloseButtonComponent', () => {
   let component: UiCloseButtonComponent;
   let fixture: ComponentFixture<UiCloseButtonComponent>;
+  let hostElement;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UiCloseButtonComponent ]
-    })
+    TestBed.configureTestingModule({ declarations: [ UiCloseButtonComponent ]})
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UiCloseButtonComponent);
     component = fixture.debugElement.componentInstance;
+    hostElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
@@ -31,21 +30,37 @@ describe('UiCloseButtonComponent', () => {
     expect(component.size).toEqual('lg');
   });
 
-  it(`when hidden is true, should be disabled, and have 'display-none'
-    class applied to button.`, () => {
+  const inputClassnameTest = (subject, inputType, inputValue, className) => {
+    component[inputType] = inputValue;
+    fixture.detectChanges();
+    expect(subject.className.includes(className)).toBeTruthy();
+  }
 
+  describe(`for inputs affecting button class,`, () => {
+    let button;
+    beforeEach(() => {
+      button = hostElement.querySelector('button.btn-close');
+    });
+    it(`when hidden is true, should be disabled, and have 'display-none'
+      class applied to button.`, () => {
+      inputClassnameTest(button, 'hidden', true, 'display-none');
+    });
+    it(`when type is 'closet-card', should have 'closet-card-icon-close' class
+      applied to button.`, () => {
+      inputClassnameTest(button, 'type', 'closet-card', 'closet-card-icon-close');
+    });
   });
 
-  it(`when type is 'closet-card', should have 'closet-card-icon-close' class
-    applied to button.`, () => {
-
-  });
-
-  it(`icon should have 'icon-lg' class when size is 'lg.'`, () => {
-
-  });
-
-  it(`icon should have 'icon-md' class when size is 'md.'`, () => {
-
+  describe(`for inputs affecting icon class,`, () => {
+    let icon;
+    beforeEach(() => {
+      icon = hostElement.querySelector('i.icon-close');
+    });
+    it(`icon should have 'icon-lg' class when size is 'lg.'`, () => {
+      inputClassnameTest(icon, 'size', 'lg', 'icon-lg');
+    });
+    it(`icon should have 'icon-md' class when size is 'md.'`, () => {
+      inputClassnameTest(icon, 'size', 'md', 'icon-md');
+    });
   });
 });
